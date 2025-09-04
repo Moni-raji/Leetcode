227da546fbd2c0355e6fa54ba3a4e8e87746f91b
Moni-raji <monisriraji19@gmail.com>
2025-09-04 12:09:05 +0530

    Time: 1 ms (99.49%) | Memory: 46.6 MB (84.66%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,17 +1,9 @@
 class Solution {
     public int[] countBits(int n) {
-        String[] arr=new String[n+1];
-        for(int i=0;i<=n;i++){
-            String s=Integer.toBinaryString(i);
-            arr[i]=s;
-        }
+        
         int r[]=new int[n+1];
         for(int i=0;i<=n;i++){
-            int count=0;
-            for(char c:arr[i].toCharArray()){
-                if(c=='1') count++;
-            }
-            r[i]=count;
+            r[i]=Integer.bitCount(i);
         }
         return r;
     }
