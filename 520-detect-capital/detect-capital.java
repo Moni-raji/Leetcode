5c00bd1ea541886b1a7a6bdaf8768171c9c113b9
Moni-raji <monisriraji19@gmail.com>
2025-09-09 22:15:22 +0530

    Time: 1 ms (94.56%) | Memory: 41.9 MB (59.65%) - LeetSync

--- 520-detect-capital/detect-capital.java
+++ 520-detect-capital/detect-capital.java
@@ -1,9 +1,13 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        if(word.matches("[A-Z]+") || word.matches("[a-z]+") || word.matches("[A-Z][a-z]+")){
-            return true;
+        int count=0;
+        for(char c:word.toCharArray()){
+            if(Character.isUpperCase(c)) count++;
         }
-        return false;
+        if(count==word.length()) return true;
+        if(count==0) return true;
+        if(count==1 && Character.isUpperCase(word.charAt(0))) return true;
 
+        return false;
     }
 }
