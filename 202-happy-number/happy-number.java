54f22ca7b64bb667cbbb6e9be3a116e06cbb9ce6
Moni-raji <monisriraji19@gmail.com>
2025-09-14 18:00:18 +0530

    Time: 1 ms (63.54%) | Memory: 41.5 MB (5.80%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set=new HashSet<>();
+        while(n!=1){
+            if(set.contains(n)){
+                return false;
+            }
+            set.add(n);
+            int sum=0;
+            while(n!=0){
+                sum+=(n%10)*(n%10);
+                n/=10;
+            }
+            n=sum;
+        }
+        return true;
+    }
+}
