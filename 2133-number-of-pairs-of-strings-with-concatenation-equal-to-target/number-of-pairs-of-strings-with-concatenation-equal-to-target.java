3f2b07357c958b9b2800de7d2348160112440a80
Moni-raji <monisriraji19@gmail.com>
2025-09-16 21:05:43 +0530

    Time: 26 ms (62.45%) | Memory: 45.3 MB (28.48%) - LeetSync

--- 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/number-of-pairs-of-strings-with-concatenation-equal-to-target.java
+++ 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/number-of-pairs-of-strings-with-concatenation-equal-to-target.java
@@ -3,9 +3,8 @@ class Solution {
         int count=0;
         for(int i=0;i<nums.length;i++){
             for(int j=0;j<nums.length;j++){
-                if(i!=j){
-                    String a=nums[i]+nums[j];
-                    if(a.equals(target)) count++;
+                if(i!=j && (nums[i]+nums[j]).equals(target)){
+                    count++;
                 }
             }
         }
