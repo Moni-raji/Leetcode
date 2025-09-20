ee34072b5dd84f474b44797d99fbb3e188281421
Moni-raji <monisriraji19@gmail.com>
2025-09-20 12:23:32 +0530

    Time: 1 ms (79.46%) | Memory: 43.4 MB (9.58%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.java
@@ -1,6 +1,5 @@
 class Solution {
     public int countSeniors(String[] details) {
-        //11 13
         int count=0;
         for(String s:details){
             int a=Integer.parseInt(s.substring(11,13));
