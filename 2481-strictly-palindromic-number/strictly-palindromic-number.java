3a989654575ffefd292c1ecfe49b815d2710e20a
Moni-raji <monisriraji19@gmail.com>
2025-09-28 17:27:49 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (84.95%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.java
@@ -1,23 +1,5 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        for(int i=2;i<=n-2;i++){
-            if(!palin(find(n,i))) return false;
-        }    
-        return true;
+        return false;
     }
-    public String find(int a,int b){
-        String r="";
-        while(a!=0){
-            r=String.valueOf(a%b)+r;
-            a/=b;
-        }
-        return r;
-    }
-    public boolean palin(String a){
-        StringBuilder sb=new StringBuilder(a);
-        sb.reverse();
-        return a.equals(sb.toString());
-
-    }
-
 }
