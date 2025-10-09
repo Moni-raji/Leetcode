b128e871b0ee5615b76f15b2ff89dc83f9483b87
Moni-raji <monisriraji19@gmail.com>
2025-10-09 09:19:43 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (8.98%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,44 +1,38 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int a=firstOccu(nums,target);
-        int b=lastOccu(nums,target);
+        int a=firstOccurence(nums,target);
+        int b=lastOccurence(nums,target);
         return new int[]{a,b};
     }
-    public int firstOccu(int[] nums,int target){
-        int r=-1;
+    public int firstOccurence(int[] arr,int target){
         int left=0;
-        int right=nums.length-1;
+        int right=arr.length-1;
+        int r=-1;
         while(left<=right){
             int mid=left+(right-left)/2;
-            if(nums[mid]==target){
+            if(arr[mid]==target){
                 r=mid;
                 right=mid-1;
             }
-            else if(nums[mid]<target){
+            else if(arr[mid]<target){
                 left=mid+1;
             }
-            else{
-                right=mid-1;
-            }
+            else right=mid-1;
         }
         return r;
     }
-    public int lastOccu(int[] nums,int target){
-        int r=-1;
+    public int lastOccurence(int[] arr,int target){
         int left=0;
-        int right=nums.length-1;
+        int right=arr.length-1;
+        int r=-1;
         while(left<=right){
             int mid=left+(right-left)/2;
-            if(nums[mid]==target){
+            if(arr[mid]==target){
                 r=mid;
                 left=mid+1;
             }
-            else if(nums[mid]<target){
-                left=mid+1;
-            }
-            else{
-                right=mid-1;
-            }
+            else if(arr[mid]<target) left=mid+1;
+            else right=mid-1;
         }
         return r;
     }
