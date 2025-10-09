cdf516235a121894555894a026ac5bb34718ae70
Moni-raji <monisriraji19@gmail.com>
2025-10-09 05:56:10 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (39.95%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -4,8 +4,8 @@ class Solution {
         int right=letters.length-1;
         while(left<=right){
             int mid=left+(right-left)/2;
-            if(target<letters[mid]) right=mid-1;
-            else left=mid+1;
+            if(letters[mid]<=target) left=mid+1;
+            else right=mid-1;
         }
         return letters[left%letters.length];
     }
