95b0640820fc6ceb32a1c2559f4a2e730bc9ae6d
Moni-raji <monisriraji19@gmail.com>
2025-10-13 10:59:12 +0530

    Time: 1 ms (99.71%) | Memory: 41.9 MB (96.85%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -6,12 +6,10 @@ class Solution {
         int carry=0;
         while(n>=0 || m>=0 || carry>0){
             int sum=carry;
-            if(n>=0) sum+=a.charAt(n)-'0';
-            if(m>=0) sum+=b.charAt(m)-'0';
+            if(n>=0) sum+=a.charAt(n--)-'0';
+            if(m>=0) sum+=b.charAt(m--)-'0';
             sb.append(sum%2);
             carry=sum/2;
-            n--;
-            m--;
         }
         return sb.reverse().toString();
     }
