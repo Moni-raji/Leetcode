1040a1fbe710e3873de2e7b05a456a3eed463dc1
Moni-raji <monisriraji19@gmail.com>
2025-10-15 11:58:37 +0530

    Time: 1 ms (45.14%) | Memory: 57.5 MB (54.53%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int temp[]=new int[k];
+        /*int temp[]=new int[k];
         int n=nums.length;
         k=k%n;
         for(int i=0;i<k;i++){
@@ -11,6 +11,18 @@ class Solution {
         }
         for(int i=0;i<k;i++){
             nums[i]=temp[i];
+        }*/
+        int n=nums.length;
+		k=k%n;
+		int a[]=new int[n];
+		for(int i=0;i<k;i++){
+		    a[i]=nums[n-k+i];
+		}
+		for(int i=k;i<n;i++){
+		    a[i]=nums[i-k];
+		}
+        for(int i=0;i<n;i++){
+            nums[i]=a[i];
         }
     }
 }
