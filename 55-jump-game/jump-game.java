d1f829bbc07a6a0064be0642403e99b0dec04409
Moni-raji <monisriraji19@gmail.com>
2025-10-15 16:52:09 +0530

    Time: 2 ms (84.59%) | Memory: 45.7 MB (57.35%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean canJump(int[] nums) {
+        if(nums.length==1) return true;
         int r=0;
         for(int i=0;i<nums.length;i++){
             if(i>r) return false;
