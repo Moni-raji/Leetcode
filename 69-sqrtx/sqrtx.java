08e69e0392635b009abd622b81d89a99321c3511
Moni-raji <monisriraji19@gmail.com>
2025-10-15 20:15:20 +0530

    Time: 1 ms (85.34%) | Memory: 40.8 MB (71.31%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,6 +1,6 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x==0 || x==1) return x;
+        /*if(x==0 || x==1) return x;
         int left=1;
         int right=x/2;
         int r=0;
@@ -14,6 +14,14 @@ class Solution {
             }
             else right=mid-1;
         }
-        return r;
+        return r;*/
+        if (x == 0) return 0;
+
+        long r = x; // Start with a rough guess (must use long to avoid overflow)
+        while (r * r > x) {
+            r = (r + x / r) / 2; // Newton's update formula
+        }
+
+        return (int) r;
     }
 }
