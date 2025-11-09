8d534c4bf9c6572f671977d4f85ca941507664c3
Moni-raji <monisriraji19@gmail.com>
2025-11-09 15:42:15 +0530

    Time: 14 ms (26.70%) | Memory: 46.5 MB (5.27%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
-        int freq[]=new int[26];
+        /*int freq[]=new int[26];
         for(int i=0;i<s.length();i++){
             freq[s.charAt(i)-'a']++;
             freq[t.charAt(i)-'a']--;
@@ -9,6 +9,12 @@ class Solution {
         for(int a:freq){
             if(a!=0) return false;
         }
-        return true;
+        return true;*/
+        Map<Character,Integer> map1=new HashMap<>();
+        Map<Character,Integer> map2=new HashMap<>();
+        for(char c:s.toCharArray()) map1.put(c,map1.getOrDefault(c,0)+1);
+        for(char c:t.toCharArray()) map2.put(c,map2.getOrDefault(c,0)+1);
+        return map1.equals(map2);
+
     }
 }
