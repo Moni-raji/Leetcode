043690b692eb129727a9159c9f4d2797529f9705
Moni-raji <monisriraji19@gmail.com>
2025-12-09 16:45:38 +0530

    Time: 8 ms (12.88%) | Memory: 48.2 MB (6.24%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int sum=0;
+        /*int sum=0;
         int len=nums.length;
         for(int i=0;i<len;i++){
             sum+=nums[i];
@@ -9,7 +9,15 @@ class Solution {
         for(int i=0;i<=len;i++){
             sum2+=i;
         }
-        return sum2-sum;
+        return sum2-sum;*/
+        int len=nums.length;
+        Arrays.sort(nums);
+        int a=Integer.MIN_VALUE;
+        if(nums[0]!=0) return 0;
+        for(int i=0;i<len-1;i++){
+            if(nums[i]!=nums[i+1]-1) return nums[i]+1;
+        }
+        return nums[len-1]+1;
 
     }
 }
