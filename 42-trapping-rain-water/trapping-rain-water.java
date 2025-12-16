96810a132e75ae80a33abfdb739ceefb89d3588a
Moni-raji <monisriraji19@gmail.com>
2025-12-16 21:06:45 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (55.70%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,7 +1,7 @@
 class Solution {
     public int trap(int[] height) {
         int n=height.length;
-        int left[]=new int[n];
+        /*int left[]=new int[n];
         left[0]=height[0];
         int right[]=new int[n];
         right[n-1]=height[n-1];
@@ -15,6 +15,22 @@ class Solution {
         for(int i=0;i<n;i++){
             water+=(Math.min(left[i],right[i])-height[i]);
         }
-        return water;
+        return water;*/
+        int total=0;
+        int left=0,right=n-1;
+        int leftm=0,rightm=0;
+        while(left<right){
+            if(height[left]<=height[right]){
+                if(leftm>height[left]) total+=leftm-height[left];
+                else leftm=height[left];
+                left++;
+            }
+            else{
+                if(rightm>height[right]) total+=rightm-height[right];
+                else rightm=height[right];
+                right--;
+            }
+        }
+        return total;
     }
 }
