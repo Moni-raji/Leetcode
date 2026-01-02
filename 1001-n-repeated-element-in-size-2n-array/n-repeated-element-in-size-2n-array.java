27d3f536a2502c59368995d5bf468ca75e8a4cda
Monisri R <monisriraji19@gmail.com>
2026-01-02 13:32:52 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (50.87%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
-        Arrays.sort(nums);
+        /*Arrays.sort(nums);
         int len=nums.length;
         int n=len/2;
         int i=0;
@@ -18,6 +18,10 @@ class Solution {
             }
             i++;
         }
-        return -1;
+        return -1;*/
+        for(int i=0;i<nums.length-2;i++){
+            if(nums[i]==nums[i+1] || nums[i]==nums[i+2]) return nums[i];
+        }
+        return nums[nums.length-1];
     }
 }
