18175b0129c07f54ac5faca9021dd292dc4fe887
Monisri R <monisriraji19@gmail.com>
2026-01-09 18:47:45 +0530

    Time: 5 ms (48.58%) | Memory: 47.5 MB (36.23%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
@@ -7,18 +7,15 @@ class Solution {
         for(int i=0;i<A.length;i++){
             if(A[i]==B[i]){
                 count++;
-                //r[i]=count;
             }
             else if(!s2.contains(A[i]) && !s1.contains(B[i])){
-                //r[i]=count;
+                
             }
             else if(s2.contains(A[i]) && s1.contains(B[i])){
                 count+=2;
-                //r[i]=count;
             }
             else if(s2.contains(A[i]) || s1.contains(B[i])){
                 count++;
-                //r[i]=count;
             }
             r[i]=count;
             s1.add(A[i]);
