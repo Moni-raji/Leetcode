641bdb7b6ab2d842d94449753399e5394769542d
Monisri R <monisriraji19@gmail.com>
2026-01-27 19:47:38 +0530

    Time: 23 ms (7.80%) | Memory: 46.4 MB (38.58%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -10,8 +10,8 @@ class Solution {
             if(a!=0) return false;
         }
         return true;*/
-        Map<Character,Integer> map1=new HashMap<>();
-        Map<Character,Integer> map2=new HashMap<>();
+        Map<Character,Integer> map1=new TreeMap<>();
+        Map<Character,Integer> map2=new TreeMap<>();
         for(char c:s.toCharArray()) map1.put(c,map1.getOrDefault(c,0)+1);
         for(char c:t.toCharArray()) map2.put(c,map2.getOrDefault(c,0)+1);
         return map1.equals(map2);
