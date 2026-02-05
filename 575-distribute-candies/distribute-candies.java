c2fd6bfa5740a0f8f12d7a2a50ebf825159e176e
Monisri R <monisriraji19@gmail.com>
2026-02-05 05:44:04 +0530

    Time: 33 ms (28.01%) | Memory: 48.7 MB (46.40%) - LeetSync

--- 575-distribute-candies/distribute-candies.java
+++ 575-distribute-candies/distribute-candies.java
@@ -1,11 +1,14 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
         int n=candyType.length/2;
-        Map<Integer,Integer> map=new HashMap<>();
+        //Map<Integer,Integer> map=new HashMap<>();
+        Set<Integer> set=new HashSet<>();
         for(int a:candyType){
-            map.put(a,map.getOrDefault(a,0)+1);
+            //map.put(a,map.getOrDefault(a,0)+1);
+            set.add(a);
         }
-        if(map.size()<n) return map.size();
+        //if(map.size()<n) return map.size();
+        if(set.size()<n) return set.size();
         return n;
     }
 }
