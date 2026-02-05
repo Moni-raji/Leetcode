1b93ffed0fbca1ddd1864422d25a6669ebea0e72
Monisri R <monisriraji19@gmail.com>
2026-02-05 10:42:08 +0530

    Time: 6 ms (32.96%) | Memory: 42.8 MB (38.34%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int count=0;
+        int s=1;
+        while(n>=s){
+            n-=s;
+            s++;
+            count++;
+        }
+        return count;
+    }
+}
