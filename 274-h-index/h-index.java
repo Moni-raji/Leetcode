c9de5f5df2cfd81839cc8f52f9b58d1e126b6df7
Monisri R <monisriraji19@gmail.com>
2026-05-26 12:49:34 +0530

    Time: 5 ms (68.29%) | Memory: 43.7 MB (15.02%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,17 +1,24 @@
 class Solution {
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
-        int n=citations.length;
-        int arr[]=new int[n];
-        int j=n-1;
+        //citations=[0 1 3 5 6]
+        int n=citations.length;//5
+        int arr[]=new int[n];//[]
+        int j=n-1;//4
         for(int i=0;i<n;i++){
             arr[i]=citations[j--];
         }
+        //arr=[6 5 3 1 0]
         int hindex=0;
         for(int i=0;i<n;i++){
-            if(!(arr[i]>=i+1)) break;
+            //if(!(arr[i]>=i+1)) break;
+            if(arr[i]<i+1) break;
             hindex=i+1;
         }
         return hindex;
     }
 }
+//the idea is 1paper should have 1citation
+//2paper should have 2 citations
+//3paper should have 3 citation
+//4paper should have 4cit this fails in input1 hence 3is o/p
