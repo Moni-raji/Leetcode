cfab7c094304e69e70f8820a8af61b96e8ecc055
Monisri R <monisriraji19@gmail.com>
2026-06-16 11:28:58 +0530

    Time: 4 ms (76.47%) | Memory: 55.3 MB (58.82%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -2,40 +2,21 @@ class Solution {
     public String processStr(String s) {
         StringBuilder sb=new StringBuilder();
         for(char c:s.toCharArray()){
-            if(Character.isLetter(c) && Character.isLowerCase(c)) sb.append(c);
+            if(c>='a' && c<='z'){
+                sb.append(c);
+            }
             else if(c=='*'){
-                if(sb.length()>=1){
-                    int n=sb.length();
-                    String a=sb.toString();
-                    sb.setLength(0);
-                    sb.append(a.substring(0,n-1));
+                if(sb.length()>0){
+                    sb.deleteCharAt(sb.length()-1);
                 }
             }
             else if(c=='#'){
-                if(sb.length()>=1){
-                    String a=sb.toString();
-                    sb.append(a);
-                }
+                sb.append(sb.toString());
             }
             else if(c=='%'){
-                String r=rev(sb.toString());
-                sb.setLength(0);
-                sb.append(r);
+                sb.reverse();
             }
         }
         return sb.toString();
-    }
-    public String rev(String s){
-        char c[]=s.toCharArray();
-        int left=0;
-        int right=c.length-1;
-        while(left<right){
-            char temp=c[left];
-            c[left]=c[right];
-            c[right]=temp;
-            left++;
-            right--;
-        }
-        return new String(c);
     }
 }
