0316eeb0b8e635fbd1c30351e34db83ff40bd501
Monisri R <monisriraji19@gmail.com>
2026-09-06 12:33:26 +0530

    Time: 42 ms (49.03%) | Memory: 46.1 MB (56.96%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,6 +1,6 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int len=1;
+        /*int len=1;
         List<Integer> al=new ArrayList<>();
         al.add(nums[0]);
         for(int i=1;i<nums.length;i++){
@@ -13,9 +13,35 @@ class Solution {
                 al.set(j,nums[i]);
             }
         }
-        return len;
+        return len;*/
+        int n = nums.length;
+
+        // dp[i] = length of LIS ending at index i
+        int[] dp = new int[n];
+
+        // Every element itself forms a subsequence of length 1
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+
+        int maxLength = 1;
+
+        for (int i = 1; i < n; i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                // nums[i] can be added after nums[j]
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+
+        return maxLength;
     }
-    public int search(List<Integer> al,int n){
+    /*public int search(List<Integer> al,int n){
         int left=0;
         int right=al.size()-1;
         while(left<right){
@@ -24,5 +50,5 @@ class Solution {
             else left=mid+1;
         }
         return left;
-    }
+    }*/
 }
