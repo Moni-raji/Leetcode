4c2f910ee6963fd7c76a94bff106dbc90329e8da
Monisri R <monisriraji19@gmail.com>
2026-09-18 18:38:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (36.04%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,6 +1,6 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n==1) return 1;
+        /*if(n==1) return 1;
         int a=1;
         int b=2;
         for(int i=3;i<=n;i++){
@@ -8,6 +8,14 @@ class Solution {
             a=b;
             b=c;
         }
-        return b;
+        return b;*/
+        if(n<=2) return n;
+        int dp[]=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
